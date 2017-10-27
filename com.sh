befb4f7be294d896ecece9e74ce5713f6e38b177
Shuya Motouchi <syu.m.5151@gmail.com>
2017-10-27 15:04:40 +0900

    Update com.sh

--- com.sh
+++ com.sh
@@ -1,8 +1,12 @@
-echo "commands:"
+#!/bin/bash
+echo "Commands to be executed on each host:"
 echo $@
 echo "==============================="
 hostfile=host.txt
 for exe_hosts in `cat ${hostfile} | grep -v ^#`
 do
+  echo "|||||||||||||||||||||||||||||||||||||||||||||"
+  echo "host:${exe_hosts}"
   ssh ${exe_hosts} -p 22 $@
+  echo "|||||||||||||||||||||||||||||||||||||||||||||"
 done
