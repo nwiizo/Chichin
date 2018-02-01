64d4421a72067e8999a5c71f72c20ae97c37d550
Shuya Motouchi <syu.m.5151@gmail.com>
2018-02-01 12:42:59 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "Commands to be executed on each host:"
 echo $@
-echo "==============================="
+echo "============================================="
 hostfile=./host.txt
 for exe_hosts in `cat ${hostfile} | grep -v ^#`
 do
-  echo "|||||||||||||||||||||||||||||||||||||||||||||"
+  echo "##############################################"
   echo "host:${exe_hosts}"
+  echo "command:${@}"
   ssh ${exe_hosts} -p 22 $@
-  echo "|||||||||||||||||||||||||||||||||||||||||||||"
 done
+echo "##############################################"
